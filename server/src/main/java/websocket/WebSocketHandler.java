9a0448ae4608d444882e24e59af88e002221f2fc
Allan Howe <allandavidhowe@gmail.com>
2024-04-04 13:22:14 -0600

    no squiggles, lets run it

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import webSocketMessages.serverMessages.NotificationMessage;
 import webSocketMessages.serverMessages.ServerMessage;
 import webSocketMessages.userCommands.UserGameCommand;
 
@@ -18,18 +19,18 @@ public class WebSocketHandler {
         UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
         switch (command.getCommandType()) {
             case JOIN_PLAYER -> joinChessGame(command.getAuthString(), session);
-            case JOIN_OBSERVER -> exit(action.visitorName());
-            case MAKE_MOVE -> ;
-            case LEAVE -> ;
-            case RESIGN -> ;
+//            case JOIN_OBSERVER -> exit(action.visitorName());
+//            case MAKE_MOVE -> ;
+//            case LEAVE -> ;
+//            case RESIGN -> ;
         }
     }
 
     private void joinChessGame(String authToken, Session session) throws IOException {
         connections.add(authToken, session);
         var message = String.format("%s authToken", authToken);
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
-        connections.broadcast(visitorName, notification);
+        var notification = new NotificationMessage(message);
+        connections.broadcast(authToken, notification);
     }
 
 }
